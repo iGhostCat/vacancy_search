df078b5194825a1935d05c9f7c0c8a182fdec694
krol.lm <krol.lm@talantiuspeh.ru>
2025-08-02 19:21:24 +0300

    work on progress

--- src/hh_api.py
+++ src/hh_api.py
@@ -1,21 +1,37 @@
 from abc import ABC, abstractmethod
 
+import requests
+
+
 class AbstractAPI(ABC):
 
     @abstractmethod
-    def connect(self):
+    def connect(self, text):
         pass
 
     @abstractmethod
-    def get_vacancies(self):
+    def get_vacancies(self, text, multi_page):
         pass
 
 
 class HH_API(AbstractAPI):
-    def __init__(self):
+    def __init__(self, page = 0):
         self.__url = 'https://api.hh.ru/vacancies'
-        self.__params = {'page' : 0, 'per_page' : 30}
+        self.__params = {'page' : page, 'per_page' : 15}
 
-    def connect(self, text, page = 0):
+    def connect(self, text):
+        self.__params['text'] = text
+        response = requests.get(self.__url, params = self.__params)
+        return response
 
+    def get_vacancies(self, text, multi_page = 0):
+        all_vacancies = []
+        while self.__params['page'] < multi_page:
+            vacancies = self.connect(text).json()['items']
+            all_vacancies.extend(vacancies)
+            self.__params['page'] += 1
+        return all_vacancies
 
+if __name__ == 'main':
+    hh = HH_API()
+    print(hh.get_vacancies('python', 0 ))
---
+++ src/json_saver.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+
+import json
+
+class AbsJSON(ABC):
+    @abstractmethod
+    def write_vacancies(self, vacancies: list[dict]):
+        pass
+
+    @abstractmethod
+    def read_vacancies(self):
+        pass
+
+    @abstractmethod
+    def delete_vacancies(self):
+        pass
+
+
+class JSONSaver(AbsJSON):
+
+    def __init__(self, filepath ='../data/vacancies.json'):
+        self.__filepath = filepath
+
+
+    def write_vacancies(self, vacancies: list[dict]):
+        vacancies_filter = []
+        for vacancy in vacancies:
+            vacancies_filter.append({"name": vacancy["name"],
+                                     "link": vacancy["alternate_url"],
+                                     "salary": vacancy["salary"],
+                                     "description": vacancy["snippet"]["requirement"]})
+        with open(self.__filepath, "w", encoding="utf-8") as file:
+            json.dump(vacancies_filter, file)
