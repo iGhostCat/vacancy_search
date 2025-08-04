15f4cb2358e3a36de6ceb6883bc2e07a61552288
iGhostCat <alfa-1002@mail.ru>
2025-08-04 15:26:51 +0300

    main on progress

--- src/main.py
+++ src/main.py
@@ -2,7 +2,16 @@ from src.vacancy import Vacancy
 from src.json_saver import JSONSaver
 from src.hh_api import HH_API
 
-hh = HH_API()
+def main():
+    query_yn = str(input("Здравствуйте! Хотите получить вакансии по запросу? Да/Нет\n"))
+    print(query_yn)
+
+
+
+
+
+main()
+'''hh = HH_API()
 vacancies = hh.get_vacancies('python', 1)
 json_saver = JSONSaver()
-json_saver.write_vacancies(vacancies)
+json_saver.write_vacancies(vacancies)'''
