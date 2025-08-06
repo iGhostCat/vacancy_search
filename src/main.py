9343b1c285b0dc55ba8f3d73a23f36aad091e7cd
iGhostCat <150450589+iGhostCat@users.noreply.github.com>
2025-08-06 22:06:09 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,14 +2,6 @@ from src.vacancy import Vacancy
 from src.json_saver import JSONSaver
 from src.hh_api import HH_API
 
-def main():
-    query_yn = str(input("Здравствуйте! Хотите получить вакансии по запросу? Да/Нет\n"))
-    if query_yn.lower() == 'нет' or query_yn.lower() == 'no':
-        print('Извините за беспокойство, до свидания!')
-        return 0
-    elif query_yn.lower() == 'да' or  query_yn.lower() == 'yes':
-        search_query = str(input("Введите ключевые слова для поиска:\n"))
-
 
 
 
