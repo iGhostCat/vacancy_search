b7dbd7146f01cefe15442cea71fa59277832d0ea
iGhostCat <alfa-1002@mail.ru>
2025-08-06 12:29:57 +0300

    main on progress

--- src/main.py
+++ src/main.py
@@ -4,7 +4,12 @@ from src.hh_api import HH_API
 
 def main():
     query_yn = str(input("Здравствуйте! Хотите получить вакансии по запросу? Да/Нет\n"))
-    print(query_yn)
+    if query_yn.lower() == 'нет' or query_yn.lower() == 'no':
+        print('Извините за беспокойство, до свидания!')
+        return 0
+    elif query_yn.lower() == 'да' or  query_yn.lower() == 'yes':
+        search_query = str(input("Введите ключевые слова для поиска:\n"))
+
 
 
 
